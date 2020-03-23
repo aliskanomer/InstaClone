cdf5ab8d7d02132989f0b60769092426953e3b8f
Ömer Alışkan <omerfarukaliskan@icloud.com>
2020-03-24 00:53:36 +0300

    Initial Commit

---
+++ InstaCloneApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstaCloneApp
+//
+//  Created by Ömer Faruk Alışkan on 24.03.20.
+//  Copyright © 2020 Ömer Faruk Alışkan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
